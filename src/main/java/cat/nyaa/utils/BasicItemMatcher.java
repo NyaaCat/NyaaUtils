153cd810fd3db4e3e93b52218098a9cc88a9c0c8
Recursive G <g.recursive@gmail.com>
2016-07-26 23:09:44 +0800

    ItemMatcher copied from HEH

---
+++ src/main/java/cat/nyaa/utils/BasicItemMatcher.java
@@ -0,0 +1,108 @@
+package cat.nyaa.utils;
+
+import org.bukkit.ChatColor;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/* TODO: support NBT matching       */
+/* TODO: item attribute modifiers   */
+/* TODO: item flags                 */
+public class BasicItemMatcher implements ISerializable{
+    @Serializable
+    public ItemStack itemTemplate = null;
+    @Serializable
+    public boolean requireExact = false; // Require item to be exactly the same. e.g. can stack together. Ignore all other rules.
+    @Serializable
+    public int minDamageValue = -2; // `-1` means `arbitrary`; `-2` means `same as template`
+    @Serializable
+    public int maxDamageValue = -2; // `-1` means `arbitrary`; `-2` means `same as template`
+    @Serializable
+    public MatchingMode enchantMatch = MatchingMode.CONTAINS;
+    @Serializable
+    public MatchingMode loreMatch = MatchingMode.EXACT_TEXT;
+    @Serializable
+    public MatchingMode nameMatch = MatchingMode.ARBITRARY;
+
+    public boolean matches(ItemStack anotherItem) {
+        ItemStack base = itemTemplate.clone();
+        ItemStack given = anotherItem.clone();
+        base.setAmount(1);
+        given.setAmount(1);
+        if (requireExact) return base.equals(given);
+        if (!base.getType().equals(given.getType())) return false;
+
+        int baseDamage = base.getDurability();
+        int givenDamage = given.getDurability();
+        if (minDamageValue == -2 && givenDamage < baseDamage) return false;
+        if (minDamageValue >= 0  && givenDamage < minDamageValue) return false;
+        if (maxDamageValue == -2 && givenDamage > baseDamage) return false;
+        if (maxDamageValue >= 0  && givenDamage > maxDamageValue) return false;
+
+        String baseDisplay = getDisplayName(base);
+        String givenDisplay = getDisplayName(given);
+        if (nameMatch == MatchingMode.EXACT && !baseDisplay.equals(givenDisplay)) return false;
+        if (nameMatch == MatchingMode.EXACT_TEXT && !ChatColor.stripColor(baseDisplay).equals(ChatColor.stripColor(givenDisplay))) return false;
+        if (nameMatch == MatchingMode.CONTAINS && !givenDisplay.contains(baseDisplay)) return false;
+        if (nameMatch == MatchingMode.CONTAINS_TEXT && !ChatColor.stripColor(givenDisplay).contains(ChatColor.stripColor(baseDisplay))) return false;
+
+        Map<Enchantment, Integer> baseEnch = base.getEnchantments();
+        Map<Enchantment, Integer> givenEnch = given.getEnchantments();
+        if (enchantMatch == MatchingMode.EXACT || enchantMatch == MatchingMode.EXACT_TEXT) {
+            if (!baseEnch.equals(givenEnch)) return false;
+        } else if (enchantMatch == MatchingMode.CONTAINS || enchantMatch == MatchingMode.CONTAINS_TEXT) {
+            for (Map.Entry<Enchantment, Integer> e : baseEnch.entrySet()) {
+                if (!givenEnch.containsKey(e.getKey()) || givenEnch.get(e.getKey()) < e.getValue())
+                    return false;
+            }
+        }
+
+        String[] baseLore = getLore(base);
+        String[] givenLore = getLore(given);
+        if (loreMatch == MatchingMode.EXACT && !Arrays.deepEquals(baseLore, givenLore)) return false;
+        if (loreMatch == MatchingMode.CONTAINS && !containStrArr(givenLore, baseLore, false)) return false;
+        if (loreMatch == MatchingMode.EXACT_TEXT) {
+            for (int i = 0; i < baseLore.length; i++) baseLore[i] = ChatColor.stripColor(baseLore[0]);
+            for (int i = 0; i < baseLore.length; i++) baseLore[i] = ChatColor.stripColor(baseLore[0]);
+            if (!Arrays.deepEquals(baseLore, givenLore)) return false;
+        }
+        if (loreMatch == MatchingMode.CONTAINS_TEXT && !containStrArr(givenLore, baseLore, true)) return false;
+
+        return true;
+    }
+
+    private String getDisplayName(ItemStack i) {
+        if (i.hasItemMeta() && i.getItemMeta().hasDisplayName()) return i.getItemMeta().getDisplayName();
+        return i.getType().name();
+    }
+
+    private String[] getLore(ItemStack i) {
+        if (!i.hasItemMeta() || !i.getItemMeta().hasLore()) return new String[0];
+        return i.getItemMeta().getLore().toArray(new String[0]);
+    }
+
+    private boolean containStrArr(String[] sample, String[] pattern, boolean stripColor) {
+        Set<String> sampleSet = new HashSet<>();
+        for (String s : sample) {
+            sampleSet.add(stripColor? ChatColor.stripColor(s): s);
+        }
+        for (String s : pattern) {
+            if (!sampleSet.contains(s))
+                return false;
+        }
+        return true;
+    }
+
+
+    enum MatchingMode {
+        EXACT,
+        EXACT_TEXT, // ignore the control chars for strings.
+        CONTAINS,
+        CONTAINS_TEXT,  // ignore the control chars for strings.
+        ARBITRARY;
+    }
+}
