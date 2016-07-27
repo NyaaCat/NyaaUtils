d87db378e2f509f85fe42d7d0cc12925cb989f57
cyilin <cyilin@users.noreply.github.com>
2016-07-27 19:23:30 +0800

    add command '/nu show'

--- src/main/java/cat/nyaa/nyaautils/CommandHandler.java
+++ src/main/java/cat/nyaa/nyaautils/CommandHandler.java
@@ -2,7 +2,9 @@ package cat.nyaa.nyaautils;
 
 import cat.nyaa.utils.CommandReceiver;
 import cat.nyaa.utils.Internationalization;
+import cat.nyaa.utils.Message;
 import org.bukkit.command.CommandSender;
+import org.bukkit.inventory.ItemStack;
 
 public class CommandHandler extends CommandReceiver<NyaaUtils> {
 
@@ -18,5 +20,11 @@ public class CommandHandler extends CommandReceiver<NyaaUtils> {
     @SubCommand(value = "enchant", permission = "nu.enchant")
     public void commandEnchant(CommandSender sender, Arguments args) {
 
+    }
+
+    @SubCommand(value = "show", permission = "nu.show")
+    public void commandShow(CommandSender sender, Arguments args) {
+        ItemStack item = getItemInHand(sender);
+        new Message("").append(item, I18n.instance.get("user.showitem.message", sender.getName())).broadcast();
     }
 }
