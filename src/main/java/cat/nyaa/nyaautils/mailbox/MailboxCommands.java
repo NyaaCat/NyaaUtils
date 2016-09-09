525e211542dfac2fef9a99094c4ab4c2da4d28c4
Recursive G <g.recursive@gmail.com>
2016-09-08 22:05:19 -0500

    sendchest & LockettePro integration

--- src/main/java/cat/nyaa/nyaautils/mailbox/MailboxCommands.java
+++ src/main/java/cat/nyaa/nyaautils/mailbox/MailboxCommands.java
@@ -3,6 +3,7 @@ package cat.nyaa.nyaautils.mailbox;
 import cat.nyaa.nyaautils.NyaaUtils;
 import cat.nyaa.utils.CommandReceiver;
 import cat.nyaa.utils.Internationalization;
+import me.crafter.mc.lockettepro.LocketteProAPI;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -109,7 +110,7 @@ public class MailboxCommands extends CommandReceiver<NyaaUtils> {
         }
 
         Player recp = plugin.getServer().getPlayer(toPlayer);
-        if (recp!=null && !recp.isOnline()) recp = null;
+        if (recp != null && !recp.isOnline()) recp = null;
         Inventory targetInventory = ((InventoryHolder) toLocation.getBlock().getState()).getInventory();
         int slot = targetInventory.firstEmpty();
         if (slot < 0) {
@@ -120,11 +121,97 @@ public class MailboxCommands extends CommandReceiver<NyaaUtils> {
         } else {
             targetInventory.setItem(slot, stack);
             p.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
-            msg(sender, "user.mailbox.mail_sent", toPlayer, (float)plugin.cfg.mailHandFee);
+            msg(sender, "user.mailbox.mail_sent", toPlayer, (float) plugin.cfg.mailHandFee);
             if (recp != null) {
                 msg(recp, "user.mailbox.mail_received", sender.getName());
             }
-            // TODO: fee
+            // TODO: log & fee
         }
+    }
+
+    @SubCommand(value = "sendchest", permission = "nu.mailbox")
+    public void sendBoxMailbox(CommandSender sender, Arguments args) {
+        Player p = asPlayer(sender);
+        String toPlayer = args.next();
+        if (toPlayer == null) {
+            msg(sender, "manual.mailbox.sendchest.usage");
+            return;
+        }
+        UUID recipient = plugin.cfg.mailbox.getUUIDbyName(toPlayer);
+        Location toLocation = plugin.cfg.mailbox.getMailboxLocation(recipient);
+
+        // Check remote mailbox
+        if (recipient != null && toLocation != null) {
+            Block b = toLocation.getBlock();
+            if (!(b.getState() instanceof InventoryHolder)) {
+                plugin.cfg.mailbox.updateLocationMapping(recipient, null);
+                toLocation = null;
+            }
+        }
+
+        if (recipient == null) {
+            msg(sender, "user.mailbox.player_no_mailbox", toPlayer);
+            return;
+        } else if (toLocation == null) {
+            msg(sender, "user.mailbox.player_no_mailbox", toPlayer);
+            Player tmp = plugin.getServer().getPlayer(toPlayer);
+            if (tmp != null && tmp.isOnline()) {
+                msg(tmp, "user.mailbox.create_mailbox_hint", sender.getName());
+            }
+            return;
+        }
+
+        final Location toLocationFinal = toLocation;
+        Player recp = plugin.getServer().getPlayer(toPlayer);
+        if (recp != null && !recp.isOnline()) recp = null;
+        final Player recpFinal = recp;
+
+        plugin.mailboxListener.registerRightClickCallback(p, 100,
+                (Location boxLocation) -> {
+                    Block b = boxLocation.getBlock();
+                    if (plugin.getServer().getPluginManager().getPlugin("LockettePro") != null) {
+                        if (LocketteProAPI.isLocked(b) && !LocketteProAPI.isUser(b, p)) {
+                            msg(p, "user.mailbox.chest_protected");
+                            return;
+                        }
+                    }
+
+                    Inventory fromInventory = ((InventoryHolder) b.getState()).getInventory();
+                    Inventory toInventory = ((InventoryHolder) toLocationFinal.getBlock().getState()).getInventory();
+                    ItemStack[] from = fromInventory.getStorageContents();
+                    ItemStack[] to = toInventory.getStorageContents();
+                    int nextSlot = 0;
+                    boolean itemMoved = false;
+                    for (int i = 0; i < from.length; i++) {
+                        if (from[i] != null && from[i].getType() != Material.AIR) {
+                            while (nextSlot < to.length && to[nextSlot] != null && to[nextSlot].getType() != Material.AIR) {
+                                nextSlot++;
+                            }
+                            if (nextSlot >= to.length) {
+                                msg(sender, "user.mailbox.recipient_no_space");
+                                if (recpFinal != null) {
+                                    msg(recpFinal, "user.mailbox.mailbox_no_space", sender.getName());
+                                }
+                                return;
+                            }
+                            to[nextSlot] = from[i].clone();
+                            itemMoved = true;
+                            from[i] = new ItemStack(Material.AIR);
+                            nextSlot++;
+                        }
+                    }
+                    if (itemMoved) {
+                        fromInventory.setStorageContents(from);
+                        toInventory.setStorageContents(to);
+                        msg(sender, "user.mailbox.mail_sent", toPlayer, (float) plugin.cfg.mailChestFee);
+                        if (recpFinal != null) {
+                            msg(recpFinal, "user.mailbox.mail_received", sender.getName());
+                        }
+                    } else {
+                        msg(sender, "user.mailbox.mail_sent_nothing");
+                    }
+                    //TODO: LOG & FEE
+                });
+        msg(p, "user.mailbox.now_right_click_send", toPlayer);
     }
 }
