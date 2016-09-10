f61f2e4cae4e3c68a5c0c9f80b7810c25c0cb0e2
Recursive G <g.recursive@gmail.com>
2016-09-09 21:46:10 -0500

    config name alias

--- src/main/java/cat/nyaa/utils/ISerializable.java
+++ src/main/java/cat/nyaa/utils/ISerializable.java
@@ -22,6 +22,7 @@ public interface ISerializable {
     @Retention(RetentionPolicy.RUNTIME)
     public @interface Serializable {
         String name() default "";
+        String[] alias() default {};
     }
 
     default void deserialize(ConfigurationSection config) {
@@ -41,16 +42,36 @@ public interface ISerializable {
             String cfgName = anno.name().equals("") ? f.getName() : anno.name();
             try {
                 Object origValue = f.get(obj);
-                Object newValue;
+                Object newValue = null;
+                boolean hasValue = false;
+                for (String key : anno.alias()) {
+                    if (config.contains(key)) {
+                        newValue = config.get(key);
+                        hasValue = true;
+                        break;
+                    }
+                }
+                if (!hasValue && config.contains(f.getName())) {
+                    newValue = config.get(f.getName());
+                    hasValue = true;
+                }
+                if (!hasValue && anno.name().length() >0 && config.contains(anno.name())) {
+                    newValue = config.get(anno.name());
+                    hasValue = true;
+                }
+                if (!hasValue) {
+                    continue;
+                }
+
                 if (f.getType().isEnum()) {
                     try {
-                        newValue = Enum.valueOf((Class<? extends Enum>) f.getType(), config.getString(cfgName));
+                        newValue = Enum.valueOf((Class<? extends Enum>) f.getType(), (String)newValue);
                     } catch (Exception ex) {
-                        newValue = origValue;
+                        ex.printStackTrace();
+                        continue;
                     }
-                } else {
-                    newValue = config.get(cfgName, origValue);
                 }
+
                 f.set(obj, newValue);
             } catch (ReflectiveOperationException ex) {
                 Bukkit.getLogger().log(Level.SEVERE, "Failed to deserialize object", ex);
@@ -64,7 +85,16 @@ public interface ISerializable {
             Serializable anno = f.getAnnotation(Serializable.class);
             if (anno == null) continue;
             f.setAccessible(true);
-            String cfgName = anno.name().equals("") ? f.getName() : anno.name();
+            String cfgName;
+            if (anno.name().equals("")) {
+                cfgName = f.getName();
+            } else {
+                cfgName = anno.name();
+                config.set(f.getName(), null);
+            }
+            for (String key : anno.alias()) {
+                config.set(key, null);
+            }
             try {
                 if (f.getType().isEnum()) {
                     Enum e = (Enum) f.get(obj);
