146d8a19883560669269213286d7d46f3068b770
Recursive G <g.recursive@gmail.com>
2016-09-16 21:39:55 -0500

    repair full

--- src/main/java/cat/nyaa/nyaautils/repair/RepairCommands.java
+++ src/main/java/cat/nyaa/nyaautils/repair/RepairCommands.java
@@ -67,6 +67,9 @@ public class RepairCommands extends CommandReceiver<NyaaUtils> {
             int repairTime = ((Repairable) item.getItemMeta()).getRepairCost();
             msg(sender, "user.repair.info_9", repairTime, info.repairLimit);
         }
+        if (info.stat == REPAIRABLE) {
+            msg(sender, "user.repair.info_10", (int)Math.ceil(item.getDurability()/(double)info.durRecovered));
+        }
     }
 
     @SubCommand(value = "hand", permission = "nu.repair")
@@ -88,12 +91,7 @@ public class RepairCommands extends CommandReceiver<NyaaUtils> {
             return;
         }
 
-        // TODO: elegant exp handling
-        int tmp = p.getTotalExperience() - info.expConsumption;
-        p.setTotalExperience(0);
-        p.setLevel(0);
-        p.setExp(0);
-        p.giveExp(tmp);
+        addPlayerExperience(p, -info.expConsumption);
 
         int dur = item.getDurability();
         dur -= info.durRecovered;
@@ -106,6 +104,71 @@ public class RepairCommands extends CommandReceiver<NyaaUtils> {
         } else {
             material.setAmount(count - 1);
             p.getInventory().setItemInOffHand(material);
+        }
+        msg(p, "user.repair.repaired");
+    }
+
+    @SubCommand(value = "full", permission = "nu.repair")
+    public void repairFull(CommandSender sender, Arguments args) {
+        ItemStack item = getItemInHand(sender);
+        ItemStack material = getItemInOffHand(sender);
+        RepairInstance info = new RepairInstance(item, plugin.cfg.repair);
+        if (info.stat != REPAIRABLE) {
+            msg(sender, "user.repair.info_3", I18n._("user.repair.unrepairable." + info.stat.name()));
+            return;
+        }
+        if (material.getType() != info.repairMaterial || material.getAmount() <= 0) {
+            msg(sender, "user.repair.material_mismatch");
+            return;
+        }
+        Player p = asPlayer(sender);
+
+        int expMax = (int)Math.floor(p.getTotalExperience()/(double)info.expConsumption);
+        int materialMax = material.getAmount();
+        int durMax = (int)Math.ceil(item.getDurability()/(double)info.durRecovered);
+        int repairAmount = Math.min(Math.min(expMax, materialMax), durMax);
+
+        addPlayerExperience(p, -info.expConsumption*repairAmount);
+        int dur = item.getDurability();
+        dur -= info.durRecovered*repairAmount;
+        if (dur < 0) dur = 0;
+        item.setDurability((short) dur);
+        p.getInventory().setItemInMainHand(item);
+        int count = material.getAmount() - repairAmount;
+        if (count <= 0) {
+            p.getInventory().setItemInOffHand(new ItemStack(Material.AIR));
+        } else {
+            material.setAmount(count);
+            p.getInventory().setItemInOffHand(material);
+        }
+        msg(p, "user.repair.repaired");
+    }
+
+    public static void addPlayerExperience(Player p, int exp) {
+        if (exp > 0) {
+            p.giveExp(exp);
+        } else if (exp < 0) {
+            exp = -exp;
+            int totalExp = p.getTotalExperience();
+            if (totalExp < exp) throw new IllegalArgumentException("Negative Exp Left");
+            totalExp -= exp;
+            int currentLevelExp = (int)(p.getExpToLevel() * p.getExp());
+            while(exp > 0) {
+                if (currentLevelExp <= 0) {
+                    if (p.getLevel() <= 0) return;
+                    p.setLevel(p.getLevel()-1);
+                    currentLevelExp = p.getExpToLevel();
+                }
+                if (exp > currentLevelExp) {
+                    exp -= currentLevelExp;
+                    currentLevelExp = 0;
+                } else {
+                    currentLevelExp -= exp;
+                    exp = 0;
+                }
+            }
+            p.setExp(currentLevelExp/(float)p.getExpToLevel());
+            p.setTotalExperience(totalExp);
         }
     }
 }
