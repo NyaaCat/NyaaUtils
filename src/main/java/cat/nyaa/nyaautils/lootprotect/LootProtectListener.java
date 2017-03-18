2ec9b831b2557ae86e41892ddc9e2521a63a0589
Recursive G <g.recursive@gmail.com>
2017-03-18 15:59:28 -0500

    Revert "lp collect exp"
    This reverts commit 96170ce38c78bcbbf7c9d28d9a563cfb408d350f.

--- src/main/java/cat/nyaa/nyaautils/lootprotect/LootProtectListener.java
+++ src/main/java/cat/nyaa/nyaautils/lootprotect/LootProtectListener.java
@@ -50,8 +50,6 @@ public class LootProtectListener implements Listener {
         if (bypassPlayer.contains(p.getUniqueId())) return;
         Map<Integer, ItemStack> leftItem =
                 p.getInventory().addItem(ev.getDrops().toArray(new ItemStack[0]));
-        p.giveExp(ev.getDroppedExp());
-        ev.setDroppedExp(0);
         ev.getDrops().clear();
         ev.getDrops().addAll(leftItem.values());
     }
