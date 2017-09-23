be56afb546d3eb1eb37c88c0c5873b46e87dfaa6
Recursive G <g.recursive@gmail.com>
2017-09-22 22:05:17 -0500

    LootProtect now works on exp

--- src/main/java/cat/nyaa/nyaautils/lootprotect/LootProtectListener.java
+++ src/main/java/cat/nyaa/nyaautils/lootprotect/LootProtectListener.java
@@ -1,17 +1,16 @@
 package cat.nyaa.nyaautils.lootprotect;
 
 import cat.nyaa.nyaautils.NyaaUtils;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerExpChangeEvent;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 public class LootProtectListener implements Listener {
     final private NyaaUtils plugin;
@@ -52,5 +51,35 @@ public class LootProtectListener implements Listener {
                 p.getInventory().addItem(ev.getDrops().toArray(new ItemStack[0]));
         ev.getDrops().clear();
         ev.getDrops().addAll(leftItem.values());
+
+        giveExp(p, ev.getDroppedExp());
+        ev.setDroppedExp(0);
+    }
+
+    private static final Random rnd = new Random();
+    /* Give exp to player, take Mending enchant into account*/
+    private static void giveExp(Player p, int amount) {
+        if (amount <= 0) return;
+        List<ItemStack> candidate = new ArrayList<>(13);
+        Arrays.stream(new ItemStack[]{
+                p.getInventory().getHelmet(),
+                p.getInventory().getChestplate(),
+                p.getInventory().getLeggings(),
+                p.getInventory().getBoots(),
+                p.getInventory().getItemInMainHand(),
+                p.getInventory().getItemInOffHand()
+        }).filter(item -> item != null && item.hasItemMeta() && item.getItemMeta().hasEnchant(Enchantment.MENDING))
+        .forEach(candidate::add);
+
+        ItemStack repair = null;
+        if (candidate.size() > 0) repair = candidate.get(rnd.nextInt(candidate.size()));
+
+        if (repair != null) {
+            int repairPoint = Math.min(amount*2, repair.getDurability());
+            repair.setDurability((short)(repair.getDurability() - repairPoint));
+            amount -= repairPoint/2;
+        }
+
+        if (amount > 0) p.giveExp(amount);
     }
 }
